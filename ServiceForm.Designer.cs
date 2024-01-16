5aab0429fba866a26795281abcacab2216ca3b7b
MaksGIT3000 <152674939+MaksGIT3000@users.noreply.github.com>
2024-01-16 15:47:53 +0400

    Add files via upload

---
+++ ServiceForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace mainWindow
+{
+    partial class ServiceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ServiceForm));
+            this.ServiceLabel = new System.Windows.Forms.Label();
+            this.TechSupport = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.Escape = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ServiceLabel
+            // 
+            this.ServiceLabel.AutoSize = true;
+            this.ServiceLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ServiceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ServiceLabel.Location = new System.Drawing.Point(22, 9);
+            this.ServiceLabel.Name = "ServiceLabel";
+            this.ServiceLabel.Size = new System.Drawing.Size(358, 55);
+            this.ServiceLabel.TabIndex = 0;
+            this.ServiceLabel.Text = "Обслуживание";
+            // 
+            // TechSupport
+            // 
+            this.TechSupport.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.TechSupport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TechSupport.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.TechSupport.Location = new System.Drawing.Point(218, 82);
+            this.TechSupport.Name = "TechSupport";
+            this.TechSupport.Size = new System.Drawing.Size(170, 34);
+            this.TechSupport.TabIndex = 2;
+            this.TechSupport.Text = "Тех.Поддержка";
+            this.TechSupport.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.button1.Location = new System.Drawing.Point(218, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Load Base Data";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(218, 221);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Активные Доставки";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.button4.Location = new System.Drawing.Point(12, 82);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(170, 34);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Back Up";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.button5.Location = new System.Drawing.Point(12, 148);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(170, 34);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Base Data Edit";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(12, 221);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(170, 34);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Turn Off";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // Escape
+            // 
+            this.Escape.BackColor = System.Drawing.Color.Red;
+            this.Escape.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Escape.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Escape.Location = new System.Drawing.Point(374, 3);
+            this.Escape.Name = "Escape";
+            this.Escape.Size = new System.Drawing.Size(22, 28);
+            this.Escape.TabIndex = 9;
+            this.Escape.Text = "X";
+            this.Escape.UseVisualStyleBackColor = false;
+            // 
+            // ServiceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.ControlBox = false;
+            this.Controls.Add(this.Escape);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TechSupport);
+            this.Controls.Add(this.ServiceLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ServiceForm";
+            this.Text = "ServiceForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ServiceLabel;
+        private System.Windows.Forms.Button TechSupport;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button Escape;
+    }
+}
