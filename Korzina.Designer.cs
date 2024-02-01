7ab950b276fae555d0a2a9e67c931aacfeb11aa8
MaksGIT3000 <152674939+MaksGIT3000@users.noreply.github.com>
2024-02-01 12:55:26 +0400

    Add files via upload

---
+++ Korzina.Designer.cs
@@ -0,0 +1,404 @@
+﻿namespace MaksProject
+{
+    partial class Korzina
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Korzina));
+            KorzinaLabel = new Label();
+            label1 = new Label();
+            NameTovar = new RichTextBox();
+            TovarTextBox1 = new RichTextBox();
+            TovarTextBox2 = new RichTextBox();
+            TovarTextBox3 = new RichTextBox();
+            TovarTextBox4 = new RichTextBox();
+            SaveBtn = new Button();
+            CloseBtn = new Button();
+            DeleteBtn = new Button();
+            DeleteBtn2 = new Button();
+            DeleteBtn3 = new Button();
+            DeleteBtn4 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            label2 = new Label();
+            button10 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            Escape = new Button();
+            SuspendLayout();
+            // 
+            // KorzinaLabel
+            // 
+            KorzinaLabel.AutoSize = true;
+            KorzinaLabel.BackColor = Color.Transparent;
+            KorzinaLabel.Font = new Font("Segoe UI", 48F, FontStyle.Regular, GraphicsUnit.Point);
+            KorzinaLabel.Location = new Point(254, 9);
+            KorzinaLabel.Name = "KorzinaLabel";
+            KorzinaLabel.Size = new Size(286, 86);
+            KorzinaLabel.TabIndex = 0;
+            KorzinaLabel.Text = "Корзина";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(95, 131);
+            label1.Name = "label1";
+            label1.Size = new Size(136, 37);
+            label1.TabIndex = 1;
+            label1.Text = "Название";
+            // 
+            // NameTovar
+            // 
+            NameTovar.Location = new Point(237, 131);
+            NameTovar.Name = "NameTovar";
+            NameTovar.Size = new Size(417, 37);
+            NameTovar.TabIndex = 2;
+            NameTovar.Text = "";
+            // 
+            // TovarTextBox1
+            // 
+            TovarTextBox1.Location = new Point(180, 243);
+            TovarTextBox1.Name = "TovarTextBox1";
+            TovarTextBox1.Size = new Size(479, 32);
+            TovarTextBox1.TabIndex = 3;
+            TovarTextBox1.Text = "";
+            // 
+            // TovarTextBox2
+            // 
+            TovarTextBox2.Location = new Point(180, 323);
+            TovarTextBox2.Name = "TovarTextBox2";
+            TovarTextBox2.Size = new Size(479, 32);
+            TovarTextBox2.TabIndex = 4;
+            TovarTextBox2.Text = "";
+            // 
+            // TovarTextBox3
+            // 
+            TovarTextBox3.Location = new Point(180, 403);
+            TovarTextBox3.Name = "TovarTextBox3";
+            TovarTextBox3.Size = new Size(479, 32);
+            TovarTextBox3.TabIndex = 5;
+            TovarTextBox3.Text = "";
+            // 
+            // TovarTextBox4
+            // 
+            TovarTextBox4.Location = new Point(180, 482);
+            TovarTextBox4.Name = "TovarTextBox4";
+            TovarTextBox4.Size = new Size(479, 32);
+            TovarTextBox4.TabIndex = 6;
+            TovarTextBox4.Text = "";
+            // 
+            // SaveBtn
+            // 
+            SaveBtn.AutoSize = true;
+            SaveBtn.BackColor = Color.Lime;
+            SaveBtn.FlatStyle = FlatStyle.Flat;
+            SaveBtn.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            SaveBtn.Location = new Point(642, 546);
+            SaveBtn.Name = "SaveBtn";
+            SaveBtn.Size = new Size(146, 42);
+            SaveBtn.TabIndex = 13;
+            SaveBtn.Text = "Сохранить";
+            SaveBtn.UseVisualStyleBackColor = false;
+            // 
+            // CloseBtn
+            // 
+            CloseBtn.AutoSize = true;
+            CloseBtn.BackColor = Color.Red;
+            CloseBtn.FlatStyle = FlatStyle.Flat;
+            CloseBtn.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            CloseBtn.Location = new Point(12, 546);
+            CloseBtn.Name = "CloseBtn";
+            CloseBtn.Size = new Size(146, 42);
+            CloseBtn.TabIndex = 14;
+            CloseBtn.Text = "Назад";
+            CloseBtn.UseVisualStyleBackColor = false;
+            // 
+            // DeleteBtn
+            // 
+            DeleteBtn.BackColor = Color.Red;
+            DeleteBtn.BackgroundImage = (Image)resources.GetObject("DeleteBtn.BackgroundImage");
+            DeleteBtn.FlatStyle = FlatStyle.Flat;
+            DeleteBtn.Location = new Point(659, 243);
+            DeleteBtn.Name = "DeleteBtn";
+            DeleteBtn.Size = new Size(40, 32);
+            DeleteBtn.TabIndex = 15;
+            DeleteBtn.UseVisualStyleBackColor = false;
+            // 
+            // DeleteBtn2
+            // 
+            DeleteBtn2.BackColor = Color.Red;
+            DeleteBtn2.BackgroundImage = (Image)resources.GetObject("DeleteBtn2.BackgroundImage");
+            DeleteBtn2.FlatStyle = FlatStyle.Flat;
+            DeleteBtn2.Location = new Point(659, 323);
+            DeleteBtn2.Name = "DeleteBtn2";
+            DeleteBtn2.Size = new Size(40, 32);
+            DeleteBtn2.TabIndex = 16;
+            DeleteBtn2.UseVisualStyleBackColor = false;
+            // 
+            // DeleteBtn3
+            // 
+            DeleteBtn3.BackColor = Color.Red;
+            DeleteBtn3.BackgroundImage = (Image)resources.GetObject("DeleteBtn3.BackgroundImage");
+            DeleteBtn3.FlatStyle = FlatStyle.Flat;
+            DeleteBtn3.Location = new Point(659, 403);
+            DeleteBtn3.Name = "DeleteBtn3";
+            DeleteBtn3.Size = new Size(40, 32);
+            DeleteBtn3.TabIndex = 17;
+            DeleteBtn3.UseVisualStyleBackColor = false;
+            // 
+            // DeleteBtn4
+            // 
+            DeleteBtn4.BackColor = Color.Red;
+            DeleteBtn4.BackgroundImage = (Image)resources.GetObject("DeleteBtn4.BackgroundImage");
+            DeleteBtn4.FlatStyle = FlatStyle.Flat;
+            DeleteBtn4.Location = new Point(659, 482);
+            DeleteBtn4.Name = "DeleteBtn4";
+            DeleteBtn4.Size = new Size(40, 32);
+            DeleteBtn4.TabIndex = 18;
+            DeleteBtn4.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.LightGray;
+            button6.BackgroundImage = (Image)resources.GetObject("button6.BackgroundImage");
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Location = new Point(140, 244);
+            button6.Name = "button6";
+            button6.Size = new Size(40, 32);
+            button6.TabIndex = 19;
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.LightGray;
+            button7.BackgroundImage = (Image)resources.GetObject("button7.BackgroundImage");
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Location = new Point(140, 323);
+            button7.Name = "button7";
+            button7.Size = new Size(40, 32);
+            button7.TabIndex = 20;
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.LightGray;
+            button8.BackgroundImage = (Image)resources.GetObject("button8.BackgroundImage");
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Location = new Point(140, 403);
+            button8.Name = "button8";
+            button8.Size = new Size(40, 32);
+            button8.TabIndex = 21;
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.LightGray;
+            button9.BackgroundImage = (Image)resources.GetObject("button9.BackgroundImage");
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Location = new Point(140, 482);
+            button9.Name = "button9";
+            button9.Size = new Size(40, 32);
+            button9.TabIndex = 22;
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(90, 242);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 33);
+            label2.TabIndex = 23;
+            label2.Text = "100";
+            label2.Click += label2_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.LightGray;
+            button10.BackgroundImage = (Image)resources.GetObject("button10.BackgroundImage");
+            button10.FlatStyle = FlatStyle.Flat;
+            button10.Location = new Point(53, 243);
+            button10.Name = "button10";
+            button10.Size = new Size(40, 32);
+            button10.TabIndex = 24;
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(90, 322);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 33);
+            label3.TabIndex = 25;
+            label3.Text = "100";
+            // 
+            // label4
+            // 
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(90, 402);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 33);
+            label4.TabIndex = 26;
+            label4.Text = "100";
+            // 
+            // label5
+            // 
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(90, 482);
+            label5.Name = "label5";
+            label5.Size = new Size(60, 33);
+            label5.TabIndex = 27;
+            label5.Text = "100";
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.LightGray;
+            button11.BackgroundImage = (Image)resources.GetObject("button11.BackgroundImage");
+            button11.FlatStyle = FlatStyle.Flat;
+            button11.Location = new Point(53, 323);
+            button11.Name = "button11";
+            button11.Size = new Size(40, 32);
+            button11.TabIndex = 28;
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.LightGray;
+            button12.BackgroundImage = (Image)resources.GetObject("button12.BackgroundImage");
+            button12.FlatStyle = FlatStyle.Flat;
+            button12.Location = new Point(53, 403);
+            button12.Name = "button12";
+            button12.Size = new Size(40, 32);
+            button12.TabIndex = 29;
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.LightGray;
+            button13.BackgroundImage = (Image)resources.GetObject("button13.BackgroundImage");
+            button13.FlatStyle = FlatStyle.Flat;
+            button13.Location = new Point(53, 483);
+            button13.Name = "button13";
+            button13.Size = new Size(40, 32);
+            button13.TabIndex = 30;
+            button13.UseVisualStyleBackColor = false;
+            // 
+            // Escape
+            // 
+            Escape.AutoSize = true;
+            Escape.BackColor = Color.Red;
+            Escape.FlatStyle = FlatStyle.Flat;
+            Escape.Location = new Point(762, 12);
+            Escape.Name = "Escape";
+            Escape.Size = new Size(26, 27);
+            Escape.TabIndex = 31;
+            Escape.Text = "X";
+            Escape.UseVisualStyleBackColor = false;
+            // 
+            // Korzina
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(800, 600);
+            ControlBox = false;
+            Controls.Add(Escape);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(DeleteBtn4);
+            Controls.Add(DeleteBtn3);
+            Controls.Add(DeleteBtn2);
+            Controls.Add(DeleteBtn);
+            Controls.Add(CloseBtn);
+            Controls.Add(SaveBtn);
+            Controls.Add(TovarTextBox4);
+            Controls.Add(TovarTextBox3);
+            Controls.Add(TovarTextBox2);
+            Controls.Add(TovarTextBox1);
+            Controls.Add(NameTovar);
+            Controls.Add(label1);
+            Controls.Add(KorzinaLabel);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Controls.Add(label5);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Korzina";
+            Text = "Korzina";
+            Load += Korzina_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label KorzinaLabel;
+        private Label label1;
+        private RichTextBox NameTovar;
+        private RichTextBox TovarTextBox1;
+        private RichTextBox TovarTextBox2;
+        private RichTextBox TovarTextBox3;
+        private RichTextBox TovarTextBox4;
+        private Button SaveBtn;
+        private Button CloseBtn;
+        private Button DeleteBtn;
+        private Button DeleteBtn2;
+        private Button DeleteBtn3;
+        private Button DeleteBtn4;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Label label2;
+        private Button button10;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button Escape;
+    }
+}
---
+++ Korzina.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaksProject
+{
+    public partial class Korzina : Form
+    {
+        public Korzina()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Korzina_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
